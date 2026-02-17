eb0b6b028193cc40f2c639493e2aa73d4a4880b2
Marcrator <edmarkbenarao29@gmail.com>
2026-02-17 15:47:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ let products = [
   { id: 19, name: "Phone Holder", price: 300, image: "images/holder.jpg" },
   { id: 20, name: "speaker", price: 300, image: "images/speaker.jpg" },
   { id: 21, name: "Cap", price: 200, image: "images/cap.jpg" },
-  { id: 22, name: "BAG", price: 200, image: "images/bag.jpg" },
+  { id: 22, name: "SCHOOL BAG", price: 200, image: "images/school bag.jpg" },
 ];
  localStorage.setItem("products", JSON.stringify(products));
 
@@ -401,6 +401,7 @@ document.addEventListener("DOMContentLoaded", () => {
   updateHeaderUI();
   showLogin();
 });
+
 
 
 
